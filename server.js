3e7b12b6aaf44249788174b64afc40b0d62ab197
foufie <shamalow423@gmail.com>
2025-07-09 10:38:35 +0200

    first commit -> accueil almost done + serv

---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).json({ error: 'Not Found'});
+});
+
+//démarrage serveur
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
